71a26902f2dbcef50c2043787fac08c37d4f1deb
AlfredoJGZ <garciazambranoalfredo@gmail.com>
2020-09-24 15:18:33 -0400

    Añadidos ajustes responsive designs

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 class Header extends Component {
     render(){
         return(
-            <div className='d-flex pt-4 justify-content-around'>
-                <div className='d-flex flex-column justify-content-around align-items-center'>
+            <div className='d-flex flex-column flex-md-row pt-4 justify-content-around justify-content-md-center align-items-center'>
+                <div className='d-flex flex-column justify-content-around align-items-center mb-4 presentacion'>
                     <h2 className='fs-40 text-center fc-main-blue'>Alfredo García</h2>
-                    <h1 className='fs-80 text-center fc-main-orange'>Desarrollo Web</h1>
+                    <h1 className='fs-40 text-center fc-main-orange font-weight-bold'>Desarrollo Web</h1>
                     <button className='rounded blue-btn' title='Contacto'>
                         Contacto
                     </button>
